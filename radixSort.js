26b1a04d4dda2c46fbf54648c111aa6d52946b3a
lynsun <814253326@qq.com>
2013-09-28 19:26:53 +0800

    Update radixSort.js

--- radixSort.js
+++ radixSort.js
@@ -1,73 +1,73 @@
-		/*
-		*@ RadixSort 基数排序
-		*@param list 待排序的自然数列表
-		*@param len	 自然数的最大位数
-		*@param radix列表项的进制，默认为10进制数
-		*/
-		function radixSort(list,len,radix){
-			radix = radix||10;
-			var bucket	= new Array(radix),
-				temp	= [],
-				rate	= 1,//rate保存当前计算的位，个位为1，十位为10
-				listLen	= list.length,//列表长度
+/*
+*@ RadixSort 基数排序
+*@param list 待排序的自然数列表
+*@param len	 自然数的最大位数
+*@param radix列表项的进制，默认为10进制数
+*/
+function radixSort(list,len,radix){
+	radix = radix||10;
+	var 	bucket	= new Array(radix),
+		temp	= [],
+		rate	= 1,//rate保存当前计算的位，个位为1，十位为10
+		listLen	= list.length,//列表长度
+		undefined;
+	
+	for(var i=0;i<len;i++){
+	
+		resetArray(bucket,0);
+		arrayCopy(list,temp);
+		
+		//计算每个列表项当前位出现的次数
+		for(var j=0;j<listLen;j++){
+			var item	= list[j],
+				key		= Math.floor(item/rate)%radix,
 				undefined;
+			/*
+			if(key<1){
+				key = 0;
+			}
+			*/
 			
-			for(var i=0;i<len;i++){
-			
-				resetArray(bucket,0);
-				arrayCopy(list,temp);
-				
-				//计算每个列表项当前位出现的次数
-				for(var j=0;j<listLen;j++){
-					var item	= list[j],
-						key		= Math.floor(item/rate)%radix,
-						undefined;
-					/*
-					if(key<1){
-						key = 0;
-					}
-					*/
-					
-					bucket[key]++;			
-				}
-				
-				//计算每个key的排序位置
-				for(j=1;j<radix;j++){
-					bucket[j] = bucket[j-1]+bucket[j];
-				}
-				
-				//按key对item进行排序
-				for(j=listLen-1;j>=0;j--){
-					item	= temp[j];
-					key		= Math.floor(item/rate)%radix;
-					list[--bucket[key]] = item;
-				}
-				console.log('按',rate,'排序结果为：',list);
-				//对下一位排序
-				rate*=radix;
-				
-			}
+			bucket[key]++;			
 		}
 		
-		
-		/*
-		*@ resetArray 重置数组元素为指定的值
-		*@param arr 要重置的数组
-		*@param val 重置的值
-		*/
-		function resetArray(arr,val){
-			for(var i=0,len=arr.length;i<len;i++){
-				arr[i] = val;
-			}
+		//计算每个key的排序位置
+		for(j=1;j<radix;j++){
+			bucket[j] = bucket[j-1]+bucket[j];
 		}
 		
-		/*
-		*@ arrayCopy 把一个数组中的值复制到另一个数组，只针对值类型，不做深度复制
-		*@param sourceArr 源数组
-		*@param destArr 目标数组
-		*/
-		function arrayCopy(sourceArr,destArr){
-			for(var i=0,len=sourceArr.length;i<len;i++){
-				destArr[i] = sourceArr[i];
-			}
+		//按key对item进行排序
+		for(j=listLen-1;j>=0;j--){
+			item	= temp[j];
+			key		= Math.floor(item/rate)%radix;
+			list[--bucket[key]] = item;
 		}
+		console.log('按',rate,'排序结果为：',list);
+		//对下一位排序
+		rate*=radix;
+		
+	}
+}
+
+
+/*
+*@ resetArray 重置数组元素为指定的值
+*@param arr 要重置的数组
+*@param val 重置的值
+*/
+function resetArray(arr,val){
+	for(var i=0,len=arr.length;i<len;i++){
+		arr[i] = val;
+	}
+}
+
+/*
+*@ arrayCopy 把一个数组中的值复制到另一个数组，只针对值类型，不做深度复制
+*@param sourceArr 源数组
+*@param destArr 目标数组
+*/
+function arrayCopy(sourceArr,destArr){
+	for(var i=0,len=sourceArr.length;i<len;i++){
+		destArr[i] = sourceArr[i];
+	}
+}
